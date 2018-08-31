735d3a0952a24dfc7a3922bb2455d8177c97b80a
WallacySebastian <wallacysebastian@hotmail.com>
2018-08-31 10:25:34 -0300

    telas adicionadas

---
+++ forca/src/forca/Controller.java
@@ -0,0 +1,11 @@
+package forca;
+
+public class Controller {
+    private String qtdLetras;
+
+    public void setQtdLetras(String string) {
+        qtdLetras = string;
+    }
+
+
+}
---
+++ forca/src/forca/TelaClientController.java
@@ -0,0 +1,35 @@
+package forca;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TelaClientController implements Initializable {
+
+    @FXML
+    private Label qtd_letras;
+
+    @FXML
+    private void loadTela(ActionEvent actionEvent) {
+
+    }
+
+    public void changeQuantidadeLetras(String string) {
+        qtd_letras.setText(string);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Controller controller = new Controller();
+
+
+    }
+
+}
