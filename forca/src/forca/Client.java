c1a9dc0a2b1f746232428f23fdad327d07982698
renan-koga <renakoga98@gmail.com>
2018-09-03 01:38:31 -0300

    Jogo funcionando (sem interface)

--- forca/src/forca/Client.java
+++ forca/src/forca/Client.java
@@ -4,159 +4,336 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintStream;
+//import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class Client {
+	public static String desenho;
+	private Socket socket;
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		// TODO Auto-generated method stub
+		
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite o IP: ");
+		String ip = ler.next();
 
-	public static Socket socket;
+		Socket cliente = new Socket(ip , 12346);
 
-//	public Socket getSocket(){
-//		return this.socket;
-//	}
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 
-	public void runClient(String ip) throws Exception{
-		this.socket = new Socket(ip, 12346);
-		System.out.println("TESTEE");
-		System.out.println("SOCKET >"+socket);
+        OutputStream ostream = cliente.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
+
+        InputStream istream = cliente.getInputStream();
+        BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+
+        String receiveMessage, sendMessage;
+        String tema, palavra;
+
+        System.out.println("\nEsperando o tema...");
+        tema = receiveFromServer(cliente);
+        System.out.println("O tema é " + tema);
+        
+        System.out.println("\nEsperando a palavra...");
+        palavra = receiveFromServer(cliente);
+        System.out.println("A palavra tem tamanho " + palavra.length() + "\n\n");
+
+        boolean logic, gameOver = false;
+        int erro;
+        int op;
+        
+        while (!gameOver) {
+        	logic = true;
+
+        	//Confirmação
+        	pwrite.println("OK!");
+        	pwrite.flush();
+        	
+        	palavra = receiveFromServer(cliente);
+        	erro = Integer.parseInt(palavra);
+        	draw(erro);
+        	System.out.println(desenho);
+        	
+        	//Confirmação
+        	pwrite.println("OK!");
+        	pwrite.flush();    
+        	
+        	palavra = receiveFromServer(cliente);
+        	System.out.println("                   " + palavra);
+        	
+        	while (logic) {
+	        	System.out.println("Digite:");
+	        	System.out.print("1 - Chutar Letra\n2 - Chutar palavra\n");
+	        	op = ler.nextInt();
+	        	
+	        	if (op == 1) {
+	        		System.out.println("\nDigite a letra: ");
+	        		sendMessage = read.readLine();  // keyboard reading
+	        		
+	            	if (sendMessage.length() == 1) {
+	            		pwrite.println(sendMessage);       // sending to server
+	            		pwrite.flush();                    // flush the data
+	            		logic = false;
+	            	}
+	            	else if (sendMessage.length() == 0) {
+	            		System.out.println("Deve digitar pelo menos uma letra!\n");
+	            	}
+	            	else {
+	            		System.out.println("Só é permitido uma letra!\n");
+	            	}
+	        	}
+	        	else if (op == 2) {
+	        		System.out.println("\nDigite a palavra: ");
+	        		sendMessage = read.readLine();  // keyboard reading
+	        		
+	        		pwrite.println(sendMessage);    // sending to server
+	        		pwrite.flush();                 // flush the data
+	        		logic = false;
+	        	}
+	        	else {
+	        		System.out.println("Opção inválida!");
+	        	}
+        	}
+        	
+        	while (true) {
+                if((receiveMessage = receiveRead.readLine()) != null) { //receive from server
+			       	if (receiveMessage.equals("Você perdeu...") ||  receiveMessage.equals("Finalmente acertou!")) {//Processo imediatamente anterior ao encerramento do programa
+			       		String finalMessage = receiveMessage;
+			       		
+			       		if (finalMessage.equals("Você perdeu...")) {//Caso o jogador tenha perdido
+			       			draw(6);					       			
+			       		}
+			       		else draw(erro);
+			       		
+				       	System.out.println(desenho);
+				       	
+				       	//Confirmação
+				       	pwrite.println("OK!");       // sending to server
+			        	pwrite.flush();
+			        	
+			        	palavra = receiveFromServer(cliente);
+			        	System.out.println("                   " + palavra);
+				       	
+			        	System.out.println("\n" + finalMessage);
+			       		gameOver = true;
+			       	}
+			       	else if (receiveMessage.equals("Continua"))  {
+			       		//Não faz nada
+			       	}
+			       	else { //Caso chute uma letra repetida
+			       		System.out.println("\n>>>>>>>>>>>>>>>>>>>> A letra \"" + receiveMessage + "\" já foi chutada <<<<<<<<<<<<<<<<<<<<\n");
+			       	}
+
+			       	break;
+                }
+        	}
+        }
+
+        ler.close();
+        cliente.close();
 	}
-
-	public String esperaTema() throws IOException {
+	
+	public static String receiveFromServer(Socket cliente) throws IOException {
 		String receiveMessage;
 
-		InputStream istream = socket.getInputStream();
+		InputStream istream = cliente.getInputStream();
         BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
 		while (true) {
 			if ((receiveMessage = receiveRead.readLine()) != null) { //receive from server
-				System.out.println("O tema é " + receiveMessage); // displaying at DOS prompt
 				break;
 			}
 		}
 		return receiveMessage;
 	}
-
-	public int esperaTamPalavra(Socket cliente) throws IOException {
-		String receiveMessage;
-		int tam;
-
-		InputStream istream = cliente.getInputStream();
-		BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
-
-		System.out.println("\nEsperando a palavra...");
-		while (true) {
-			if ((receiveMessage = receiveRead.readLine()) != null) //receive from server
-			{
-				tam = receiveMessage.length();
-				System.out.println("A palavra tem tamanho "+tam+"\n\n");
-				//          System.out.println(receiveMessage); // displaying at DOS prompt
+	
+	public static void draw(int erro) {
+		String str;
+		
+		switch (erro) {
+			case 0:
+				str = "      ***************\n";
+				str+= "      ***************\n";
+				str+= "      ***        *** \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "     *****           \n";
+//				str+= str1;
+				desenho = str;
+				
 				break;
-			}
+				
+			case 1:
+				str = "      ***************\n";
+				str+= "      ***************\n";
+				str+= "      ***        *** \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***       *   * \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "     *****           \n";
+//				str+= str1;
+				desenho = str;
+				
+				break;
+				
+			case 2:
+				str = "      ***************\n";
+				str+= "      ***************\n";
+				str+= "      ***        *** \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***       *   * \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***         |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "     *****           \n";
+//				str+= str1;
+				desenho = str;
+				
+				break;
+							
+			case 3:
+				str = "      ***************\n";
+				str+= "      ***************\n";
+				str+= "      ***        *** \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***       *   * \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***         |   \n";
+				str+= "      ***        /|   \n";
+				str+= "      ***       / |   \n";
+				str+= "      ***      *  |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "     *****           \n";
+//				str+= str1;
+				desenho = str;
+
+				break;
+				
+			case 4:
+				str = "      ***************\n";
+				str+= "      ***************\n";
+				str+= "      ***        *** \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***       *   * \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***         |   \n";
+				str+= "      ***        /|\\   \n";
+				str+= "      ***       / | \\  \n";
+				str+= "      ***      *  |  * \n";
+				str+= "      ***         |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "     *****           \n";
+//				str+= str1;
+				desenho = str;
+
+				break;
+				
+			case 5:
+				str = "      ***************\n";
+				str+= "      ***************\n";
+				str+= "      ***        *** \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***       *   * \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***         |   \n";
+				str+= "      ***        /|\\   \n";
+				str+= "      ***       / | \\  \n";
+				str+= "      ***      *  |  * \n";
+				str+= "      ***         |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***        /    \n";
+				str+= "      ***       /     \n";
+				str+= "      ***     _/      \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+//				str+= str1;
+				desenho = str;
+
+				break;
+				
+			case 6:
+				str = "      ***************\n";
+				str+= "      ***************\n";
+				str+= "      ***        *** \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***       *   * \n";
+				str+= "      ***        * *  \n";
+				str+= "      ***         |   \n";
+				str+= "      ***        /|\\   \n";
+				str+= "      ***       / | \\  \n";
+				str+= "      ***      *  |  * \n";
+				str+= "      ***         |   \n";
+				str+= "      ***         |   \n";
+				str+= "      ***        / \\   \n";
+				str+= "      ***       /   \\  \n";
+				str+= "      ***     _/     \\_ \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+				str+= "      ***            \n";
+//				str+= str1;
+				desenho = str;
+
+				break;
+				
+			default:
+				break;
+				
 		}
-		return tam;
 	}
-//		// TODO Auto-generated method stub
-//		Scanner ler = new Scanner(System.in);
-//		System.out.println("Digite seu nick de Jogador: ");
-//		String nick = ler.next();
-//
-//		Socket cliente = new Socket("191.52.64.26", 12346);
-//        System.out.println("O Jogador "+nick+" se conectou ao Jogo!");
-//
-//        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-////      PrintStream saida = new PrintStream(cliente.getOutputStream());
-//
-//
-//        OutputStream ostream = cliente.getOutputStream();
-//        PrintWriter pwrite = new PrintWriter(ostream, true);
-//
-//                                 // receiving from server ( receiveRead  object)
-//        InputStream istream = cliente.getInputStream();
-//        BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
-//
-////        System.out.println("Start the chitchat, type and press Enter key");
-//
-//        String receiveMessage, sendMessage;
-//
-//        System.out.println("\nEsperando o tema...");
-//        while (true) {
-//             if((receiveMessage = receiveRead.readLine()) != null) { //receive from server
-//            	 System.out.println("O tema é "+receiveMessage); // displaying at DOS prompt
-//            	 break;
-//             }
-//
-//        }
-//        System.out.println("\nEsperando a palavra...");
-//        while (true) {
-//        	if ((receiveMessage = receiveRead.readLine()) != null) //receive from server
-//        	{
-//        		int tam = receiveMessage.length();
-//			   	System.out.println("A palavra tem tamanho "+tam+"\n\n");
-//	//          System.out.println(receiveMessage); // displaying at DOS prompt
-//			   	break;
-//        	}
-//        }
-//
-//        boolean logic = true;
-//        boolean logic2;
-//        while (logic) {
-//        	logic2 = false;
-//        	System.out.print("1 - Chutar Letra\n2 - Chutar palavra\n");
-//        	int op = ler.nextInt();
-//        	System.out.println("Digite:");
-//        	if (op == 1) {
-//        		sendMessage = read.readLine();  // keyboard reading
-//            	if (sendMessage.length() == 1) {
-//            		pwrite.println(sendMessage);       // sending to server
-//            		pwrite.flush();                    // flush the data
-//            	}
-//            	else if (sendMessage.length() == 0) {
-//            		System.out.println("Deve digitar pelo menos uma letra!\n");
-//            		logic2 = true;
-//            	}
-//            	else {
-//            		System.out.println("Só é permitido uma letra!\n");
-//            		logic2 = true;
-//            	}
-//        	}
-//        	else if (op == 2) {
-//        		sendMessage = read.readLine();  // keyboard reading
-//        		pwrite.println(sendMessage);    // sending to server
-//        		pwrite.flush();                 // flush the data
-//        	}
-//        	else {
-//        		System.out.println("Opção inválida!");
-//        	}
-//
-//        	while (true && !logic2) {
-//        		if ((receiveMessage = receiveRead.readLine()) != null) { //receive from server
-//        			System.out.println(receiveMessage + "\n"); // displaying at DOS prompt
-//
-//			       	while (true) {
-//		                if((receiveMessage = receiveRead.readLine()) != null) { //receive from server
-//					       	if (receiveMessage.equals("Se fudeu...") ||  receiveMessage.equals("Finalmente acertou!")) {
-//						       	System.out.println(receiveMessage); // displaying at DOS prompt
-//					       		logic = false;
-//					       	}
-//
-//					       	break;
-//		                }
-//			       	}
-//
-//			       	break;
-//        		}
-//        	}
-//        }
-//
-//        ler.close();
-
-
 }
--- forca/src/forca/Server.java
+++ forca/src/forca/Server.java
@@ -1,108 +1,173 @@
 package forca;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.*;
-import java.util.Scanner;
+import java.util.Hashtable;
 
 public class Server {
 	public static String str1;
 	private static int acertos=0;
+	private static int port = 12346;
 	
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
+		Hashtable letras = new Hashtable(30);
 		int erro=0;
-		ServerSocket server = new ServerSocket(12346);
-		System.out.println("Porta 12346 aberta!");
+		String err;
+		
+		ServerSocket server = new ServerSocket(port);
+		System.out.println("Porta " + port + " aberta!");
 		System.out.println("Esperando Jogador...");
-		Socket client = server.accept();
-		System.out.println("Jogador: "+client.getInetAddress().getHostAddress()+" conectado na porta 12345");
+		Socket client = server.accept();		
+		System.out.println("Jogador: " + client.getInetAddress().getHostAddress() + " conectado na porta " + port);
 		
 		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        // sending to client (pwrite object)
-		OutputStream ostream = client.getOutputStream(); 
+		OutputStream ostream = client.getOutputStream();
 		PrintWriter pwrite = new PrintWriter(ostream, true);
 
-            // receiving from server ( receiveRead  object)
 		InputStream istream = client.getInputStream();
 		BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
-		String receiveMessage, sendMessage; 
+		String receiveMessage, sendMessage;
 		
-		System.out.println("Digite o tema: ");
-		sendMessage = read.readLine(); 
-		pwrite.println(sendMessage);             
+		System.out.println("\n\nDigite o tema: ");
+		sendMessage = read.readLine();
+		pwrite.println(sendMessage);
 		pwrite.flush();
 		
 		System.out.println("Digite a palavra: ");
-		sendMessage = read.readLine(); 
-		pwrite.println(sendMessage);             
+		sendMessage = read.readLine();
+		pwrite.println(sendMessage);
 		pwrite.flush();
 		
 		sendMessage = sendMessage.toLowerCase();
-//		String string = sendMessage;
-		formataString(sendMessage.length());//Coloca underlines
-//				String str2 = str1; //String cópia para comparações
+		
+//		Coloca underlines
+		formataString(sendMessage.length());
 		
 		while (true) {
+			while (true) {//Aguarda confirmação
+				if ((receiveMessage = receiveRead.readLine()) != null) { //receive from server
+					break;
+				}
+			}
+			
+			//Envia a quantidade de erros atual
+			err = convertErro(erro);
+			pwrite.println(err);
+			pwrite.flush();
+			
+			while (true) {//Aguarda confirmação
+				if ((receiveMessage = receiveRead.readLine()) != null) { //receive from server
+					break;
+				}
+			}
+			
+			//Envia a string correspondente a palavra a ser descoberta
+			pwrite.println(str1);             
+			pwrite.flush();
+			
 			if ((receiveMessage = receiveRead.readLine()) != null) {
 				if (receiveMessage.length()>1) {//Se jogador optou por digitar palavra completa
 					
-					if (receiveMessage.equals(sendMessage)) {
-						pwrite.println(sendMessage);
-						pwrite.flush();
+					if (receiveMessage.equals(sendMessage)) {//Caso o jogador tenha acertado a palavra			
+						acertouPalavra(receiveMessage);
 						
-						//Caso o jogador tenha acertado a palavra
 						pwrite.println("Finalmente acertou!");
 						pwrite.flush();
-						break;
-					}
-					else {
-						pwrite.println(str1);
+						
+						while (true) {//Aguarda confirmação
+							if ((receiveMessage = receiveRead.readLine()) != null) { //receive from server
+								break;
+							}
+						}
+						
+						//Envia a string correspondente a palavra a ser descoberta
+						pwrite.print(str1);
 						pwrite.flush();
 						
-						pwrite.println("Se fudeu...");
+						break;
+					}
+					else {//Se o jogador perdeu...
+						pwrite.println("Você perdeu...");
 						pwrite.flush();
+						
+						while (true) {//Aguarda confirmação
+							if ((receiveMessage = receiveRead.readLine()) != null) { //receive from server
+								break;
+							}
+						}
+						
+						//Envia a string correspondente a palavra a ser descoberta
+						pwrite.print(str1);
+						pwrite.flush();
+						
 						break;
 					}
 				}
 				else {//Se jogador optou por digitar letra
 					
 					//Pega char
-					char c = receiveMessage.charAt(0); 
-					//Verifica se existe a letra na string
-					if(!trataRetorno(c, sendMessage)) {								
-						erro++;
+					char c = receiveMessage.charAt(0);
+					boolean letraRepetida = false;
+					
+					if (!letras.contains(c)) {						
+						letras.put((int) c, (char) c); 
+
+						//Verifica se existe a letra na string
+						if(!trataRetorno(c, sendMessage)) {								
+							erro++;
+						}
 					}
+					else letraRepetida = true;
 					
-					pwrite.println(str1);
-					pwrite.flush();
-					
-					if (erro == 6) {
-						pwrite.println("Se fudeu...");             
+					if (erro == 6) {//Se o jogador perdeu...
+						pwrite.println("Você perdeu...");             
 						pwrite.flush();
-//								System.out.println("Encerra");
+						
+						while (true) {//Aguarda confirmação
+							if ((receiveMessage = receiveRead.readLine()) != null) { //receive from server
+								break;
+							}
+						}
+						
+						//Envia a string correspondente a palavra a ser descoberta
+						pwrite.print(str1);
+						pwrite.flush();
+						
 						break;
 					}
 					else {
-						if (acertos == sendMessage.length()) {
-							//Caso o jogador tenha acertado a palavra		
-							pwrite.println("Finalmente acertou!");             
+						if (acertos == sendMessage.length()) {//Caso o jogador tenha acertado a palavra		
+							pwrite.println("Finalmente acertou!");
 							pwrite.flush();
+							
+							while (true) {//Aguarda confirmação
+								if ((receiveMessage = receiveRead.readLine()) != null) { //receive from server
+									break;
+								}
+							}
+							
+							//Envia a string correspondente a palavra a ser descoberta
+							pwrite.print(str1);
+							pwrite.flush();
+							
 							break;
 						}
-						else {
-							//Printa na tela do jogador a string formatada
-							pwrite.println("Continua");
-							pwrite.flush();
+						else {//Printa na tela do jogador a string formatada
+							if (letraRepetida) {//Envia letra repetida para ser exibida na tela do jogador
+								pwrite.println(c);
+								pwrite.flush();
+							}
+							else {
+								pwrite.println("Continua");
+								pwrite.flush();
+							}
 						}
-//						str2 = str1;
 					}
 				}
 			}	
@@ -124,6 +189,15 @@ public class Server {
 			}
 		}
 		return logic;
+	} 
+	
+	public static void acertouPalavra(String palavra) {
+		str1 = "";
+		
+		for (int i=0; i<palavra.length(); i++) {
+			str1 += palavra.charAt(i);
+			str1 += " ";
+		}
 	}
 	
 	public static void formataString(int tam) {
@@ -132,12 +206,34 @@ public class Server {
 			str1 = str1.concat("_ ");
 		}
 		str1 = str1.concat("_");
-//		return str1;
 	}
 	
-	public static String espacoString(String str) {
-		
-		return null;
+	public static String convertErro(int erro) {
+		switch(erro) {
+			case 0:
+				return "0";
+			
+			case 1:
+				return "1";
+				
+			case 2:
+				return "2";
+			
+			case 3:
+				return "3";
+				
+			case 4:
+				return "4";
+			
+			case 5:
+				return "5";
+				
+			case 6:
+				return "6";
+				
+			default:
+				return null;
+		}
 	}
 
 }
